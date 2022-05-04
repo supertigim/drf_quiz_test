d49777aec7448c34add3dfde899be39435039aa1
Jong Yoon Kim <jyoon95@gmail.com>
2022-05-04 13:56:58 +0900

    Ready for deploy

---
+++ quiz/views.py
@@ -0,0 +1,17 @@
+from rest_framework.response import Response 
+from rest_framework.decorators import api_view
+from .models import Quiz
+from .serialize import QuizSerializer
+import random
+
+# Create your views here.
+@api_view(['GET'])
+def helloAPI(request):
+    return Response("hello world!")
+
+@api_view(['GET'])
+def randomQuiz(request, id):
+    totalQuizzes = Quiz.objects.all()
+    randomQuizzes = random.sample(list(totalQuizzes), id)
+    serializer = QuizSerializer(randomQuizzes, many=True)
+    return Response(serializer.data)
